aa000c5ea85a44793010b1f03a74f45546e2c27c
Patrice Chalin <pchalin@gmail.com>
2018-07-17 15:30:57 -0400

    Natalie's feedback

--- test/yaml_node_wrapper_test.dart
+++ test/yaml_node_wrapper_test.dart
@@ -53,19 +53,19 @@ main() {
         }));
 
     expect(map.span, isNullSpan(isNull));
-    expect(map["list"], TypeMatcher<YamlList>());
-    expect(map["list"].nodes[0], TypeMatcher<YamlScalar>());
+    expect(map["list"], new TypeMatcher<YamlList>());
+    expect(map["list"].nodes[0], new TypeMatcher<YamlScalar>());
     expect(map["list"].span, isNullSpan(isNull));
-    expect(map["map"], TypeMatcher<YamlMap>());
-    expect(map["map"].nodes["foo"], TypeMatcher<YamlScalar>());
-    expect(map["map"]["nested"], TypeMatcher<YamlList>());
+    expect(map["map"], new TypeMatcher<YamlMap>());
+    expect(map["map"].nodes["foo"], new TypeMatcher<YamlScalar>());
+    expect(map["map"]["nested"], new TypeMatcher<YamlList>());
     expect(map["map"].span, isNullSpan(isNull));
-    expect(map.nodes["scalar"], TypeMatcher<YamlScalar>());
+    expect(map.nodes["scalar"], new TypeMatcher<YamlScalar>());
     expect(map.nodes["scalar"].value, "value");
     expect(map.nodes["scalar"].span, isNullSpan(isNull));
     expect(map["scalar"], "value");
     expect(map.keys, unorderedEquals(["list", "map", "scalar"]));
-    expect(map.nodes.keys, everyElement(TypeMatcher<YamlScalar>()));
+    expect(map.nodes.keys, everyElement(new TypeMatcher<YamlScalar>()));
     expect(map.nodes[new YamlScalar.wrap("list")], equals([1, 2, 3]));
   });
 
@@ -108,14 +108,14 @@ main() {
         ]));
 
     expect(list.span, isNullSpan(isNull));
-    expect(list[0], TypeMatcher<YamlList>());
-    expect(list[0].nodes[0], TypeMatcher<YamlScalar>());
+    expect(list[0], new TypeMatcher<YamlList>());
+    expect(list[0].nodes[0], new TypeMatcher<YamlScalar>());
     expect(list[0].span, isNullSpan(isNull));
-    expect(list[1], TypeMatcher<YamlMap>());
-    expect(list[1].nodes["foo"], TypeMatcher<YamlScalar>());
-    expect(list[1]["nested"], TypeMatcher<YamlList>());
+    expect(list[1], new TypeMatcher<YamlMap>());
+    expect(list[1].nodes["foo"], new TypeMatcher<YamlScalar>());
+    expect(list[1]["nested"], new TypeMatcher<YamlList>());
     expect(list[1].span, isNullSpan(isNull));
-    expect(list.nodes[2], TypeMatcher<YamlScalar>());
+    expect(list.nodes[2], new TypeMatcher<YamlScalar>());
     expect(list.nodes[2].value, "value");
     expect(list.nodes[2].span, isNullSpan(isNull));
     expect(list[2], "value");
@@ -153,7 +153,7 @@ main() {
 }
 
 Matcher isNullSpan(sourceUrl) => predicate((span) {
-      expect(span, TypeMatcher<SourceSpan>());
+      expect(span, new TypeMatcher<SourceSpan>());
       expect(span.length, equals(0));
       expect(span.text, isEmpty);
       expect(span.start, equals(span.end));
