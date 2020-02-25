eeecbc502f28d711e070a9da9b871247945ac4fd
Sahil Bondre <sahilbondre@gmail.com>
2020-02-25 13:39:09 -0800

    Refactor to real enums (#62)

--- lib/src/token.dart
+++ lib/src/token.dart
@@ -129,38 +129,31 @@ class ScalarToken implements Token {
 }
 
 /// An enum of types of [Token] object.
-class TokenType {
-  static const STREAM_START = TokenType._('STREAM_START');
-  static const STREAM_END = TokenType._('STREAM_END');
+enum TokenType {
+  STREAM_START,
+  STREAM_END,
 
-  static const VERSION_DIRECTIVE = TokenType._('VERSION_DIRECTIVE');
-  static const TAG_DIRECTIVE = TokenType._('TAG_DIRECTIVE');
-  static const DOCUMENT_START = TokenType._('DOCUMENT_START');
-  static const DOCUMENT_END = TokenType._('DOCUMENT_END');
+  VERSION_DIRECTIVE,
+  TAG_DIRECTIVE,
+  DOCUMENT_START,
+  DOCUMENT_END,
 
-  static const BLOCK_SEQUENCE_START = TokenType._('BLOCK_SEQUENCE_START');
-  static const BLOCK_MAPPING_START = TokenType._('BLOCK_MAPPING_START');
-  static const BLOCK_END = TokenType._('BLOCK_END');
+  BLOCK_SEQUENCE_START,
+  BLOCK_MAPPING_START,
+  BLOCK_END,
 
-  static const FLOW_SEQUENCE_START = TokenType._('FLOW_SEQUENCE_START');
-  static const FLOW_SEQUENCE_END = TokenType._('FLOW_SEQUENCE_END');
-  static const FLOW_MAPPING_START = TokenType._('FLOW_MAPPING_START');
-  static const FLOW_MAPPING_END = TokenType._('FLOW_MAPPING_END');
+  FLOW_SEQUENCE_START,
+  FLOW_SEQUENCE_END,
+  FLOW_MAPPING_START,
+  FLOW_MAPPING_END,
 
-  static const BLOCK_ENTRY = TokenType._('BLOCK_ENTRY');
-  static const FLOW_ENTRY = TokenType._('FLOW_ENTRY');
-  static const KEY = TokenType._('KEY');
-  static const VALUE = TokenType._('VALUE');
+  BLOCK_ENTRY,
+  FLOW_ENTRY,
+  KEY,
+  VALUE,
 
-  static const ALIAS = TokenType._('ALIAS');
-  static const ANCHOR = TokenType._('ANCHOR');
-  static const TAG = TokenType._('TAG');
-  static const SCALAR = TokenType._('SCALAR');
-
-  final String name;
-
-  const TokenType._(this.name);
-
-  @override
-  String toString() => name;
+  ALIAS,
+  ANCHOR,
+  TAG,
+  SCALAR
 }
