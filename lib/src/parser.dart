af18057895db31cbaa4187bd31f9c6ee824597f3
Adarsh Maurya <chotuport@gmail.com>
2020-04-21 10:42:17 +0530

    fix for 49
    fixed issue 49 and added some tests for validation

--- lib/src/parser.dart
+++ lib/src/parser.dart
@@ -348,12 +348,13 @@ class Parser {
     var token = _scanner.peek();
 
     if (token.type == TokenType.blockEntry) {
+      var prevToken = token;
       token = _scanner.advance();
 
       if (token.type == TokenType.blockEntry ||
           token.type == TokenType.blockEnd) {
         _state = _State.BLOCK_SEQUENCE_ENTRY;
-        return _processEmptyScalar(token.span.end);
+        return _processEmptyScalar(prevToken.span.end);
       } else {
         _states.add(_State.BLOCK_SEQUENCE_ENTRY);
         return _parseNode(block: true);
