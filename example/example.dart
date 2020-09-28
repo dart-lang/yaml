0d4eb95a379772060dbc0362fedea46c9dd1e785
Francesco Iapicca <iapicca@gmail.com>
2020-09-28 15:07:49 +0300

    fixes

--- example/example.dart
+++ example/example.dart
@@ -43,51 +43,55 @@ morecomplex:
 YamlMap get yamlMap {
   final _yamlDocument = loadYamlDocument(_yaml);
   final _yamlNode = _yamlDocument.contents;
-  return _yamlNode.value;
+  return _yamlNode.value as YamlMap;
 }
 
 void main() {
   final _yamlMap = yamlMap;
   {
     /// this reads a [String] from [YamlMap] providing a [key]
-    final String _message = _yamlMap['message'];
+    final _message = _yamlMap['message'] as String;
     print(_message);
   }
   {
     /// this reads a [YamlMap] from [YamlMap] providing a [key]
-    final YamlMap _map = _yamlMap['map'];
-    final String _message = _map['message'];
-    final String _another = _map['another'];
+    final _map = _yamlMap['map'] as YamlMap;
+    final _message = _map['message'] as String;
+    final _another = _map['another'] as String;
     print(_message);
     print(_another);
   }
   {
     /// this reads a [YamlList] from [YamlMap] providing a [key]
-    final YamlList yamlList = _yamlMap['list'];
-    final _integers = <int>[for (int i in yamlList) i];
-    print('${_integers}');
+    final yamlList = _yamlMap['list'] as YamlList;
+    final _integers = <int>[for (YamlNode i in yamlList.nodes) i.value as int];
+    print('$_integers');
   }
   {
     /// gets from [YamlMap] a [YamlMap] which has as values
     /// a [String] and a [List<int>]
-    final YamlMap _complex = _yamlMap['complex'];
-    final String _message = _complex['message'];
+    final _complex = _yamlMap['complex'] as YamlMap;
+    final _message = _complex['message'] as String;
     print(_message);
-    final YamlList _yamlList = _complex['list'];
-    final _integers = <int>[for (int i in _yamlList) i];
-    print('${_integers}');
+    final yamlList = _complex['list'] as YamlList;
+    final _integers = <int>[for (YamlNode i in yamlList.nodes) i.value as int];
+    print('$_integers');
   }
   {
     /// gets from [YamlMap] a [List<YamlMap>] each of those have as values
     /// a [String] and a [List<int>]
-    final YamlList _moreComplex = _yamlMap['morecomplex'];
-    final _maps = <YamlMap>[for (dynamic d in _moreComplex) d['complex']];
+    final _moreComplex = _yamlMap['morecomplex'] as YamlList;
+    final _maps = <YamlMap>[
+      for (YamlNode d in _moreComplex.nodes) d.value['complex'] as YamlMap
+    ];
     for (var complex in _maps) {
-      final String _message = complex['message'];
+      final _message = complex['message'] as String;
       print(_message);
-      final YamlList _yamlList = complex['list'];
-      final _integers = <int>[for (int i in _yamlList) i];
-      print('${_integers}');
+      final _yamlList = complex['list'] as YamlList;
+      final _integers = <int>[
+        for (YamlNode i in _yamlList.nodes) i.value as int
+      ];
+      print('$_integers');
     }
   }
 }
