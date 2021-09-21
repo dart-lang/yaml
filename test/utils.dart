3bb811f8728348f604a5957d69c68fc00b46f827
Jonas Finnemann Jensen <jonasfj@google.com>
2021-09-21 17:20:03 +0200

    Simplify licensing

--- test/utils.dart
+++ test/utils.dart
@@ -1,6 +1,9 @@
-// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
+// Copyright (c) 2014, the Dart project authors.
+// Copyright (c) 2006, Kirill Simonov.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
 
 import 'package:test/test.dart';
 import 'package:yaml/src/equality.dart' as equality;
